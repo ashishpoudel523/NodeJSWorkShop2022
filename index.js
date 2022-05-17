0d9eb861794cbc1074903bfab01ea202ddc5cabf
Ashish Poudel <ashishpoudel523@gmail.com>
2022-05-17 19:17:46 +0545

    ORM operations

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ connection.define("users", {
         type: DataTypes.INTEGER;
     }
 })
+
+
+get() {
+    return this.get
+}
+
+
+ReadableStreamDefaultController.get("/search/by", async {req, res} =>{
+    const {location} = req.query
+
+   const data = await userModel.findAll{{
+        where:{
+            location:{
+                [Op.like]: `%${location}%`
+            },
+        },
+    }}
+})
