0084fae9c19f682fc17139ba53e3c5ba77dded1f
Ashish Poudel <ashishpoudel523@gmail.com>
2022-05-17 18:40:45 +0545

    NVM

--- index.js
+++ index.js
@@ -1,48 +1,11 @@
-import express from "express"
+import {
+    DataTypes
+} from "sequalize"
 
-import mysql from "mysql2"
+import connection from "./index.js"
 
-const app = express();
-
-let connection
-
-//update
-
-app.put("/user/update/:id", {
-    req,
-    res
-} => {
-    const {
-        id
-    } = req.params
-
-    if (id) {
-        const {
-            username,
-            location
-        } = req.body
-
-        connection.query {
-            "UPDATE users SET username 
-        }
-
-    }
-})
-
-
-//delete
-app.delete("/user/delete/:id", {
-    req,
-    res
-} => {
-    const {
-        id
-    } = req.params
-    if {
-        id
-    } {
-
-    } else {
-        res.status(403).json
+connection.define("users", {
+    id: {
+        type: DataTypes.INTEGER;
     }
 })
