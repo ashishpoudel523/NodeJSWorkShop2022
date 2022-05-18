a56af3e3ccecb86acbcf36e8800f4cc96d3b78e3
Ashish Poudel <ashishpoudel523@gmail.com>
2022-05-18 22:04:42 +0545

    objects in nodejs

--- index.js
+++ index.js
@@ -6,3 +6,32 @@ function add(a, b) {
 }
 
 console.log(add(1, 4))
+
+
+
+
+//global variable 
+global.companyName = "Ashish"
+
+console.log(companyName)
+
+
+function print() {
+    console.log(companyName)
+}
+
+print()
+
+
+console.log(process.USER)
+
+
+setInterval(() => console.log("hello"), 1000)
+
+
+
+console.log(__filename)
+
+console.log(__dirname)
+
+require("app.js")
