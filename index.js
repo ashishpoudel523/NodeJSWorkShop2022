70563d3fbefb11b8573bb87ad8692907e4cf01fc
Ashish Poudel <ashishpoudel523@gmail.com>
2022-05-25 23:32:56 +0545

    mongodb integrated

--- index.js
+++ index.js
@@ -35,3 +35,26 @@ console.log(__filename)
 console.log(__dirname)
 
 const add = require("./app")
+
+
+
+
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import blogRouter from "./routes/blogRouter.js";
+
+const app = express();
+app.use("/blog", blogRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(8000, async () => {
+  console.log("Server has started ");
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_URL);
+    console.log("Connected to DB 🚀");
+  } catch (err) {
+    console.log(err);
+  }
+});
