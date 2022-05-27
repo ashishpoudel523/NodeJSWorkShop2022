435def0f177a3381b83892798e377d8df921e9be
Ashish Poudel <ashishpoudel523@gmail.com>
2022-05-27 18:28:52 +0545

    template engine

---
+++ server.js
@@ -0,0 +1,11 @@
+// Template Engine
+
+
+// Template enignes are the way to create dynamic HTML pages in a modular pattern.Engine
+
+// They are not basically the hardware the hardware engines but a library files that helps us to render other codes in our HTML page; 
+
+
+app.get {
+
+}
