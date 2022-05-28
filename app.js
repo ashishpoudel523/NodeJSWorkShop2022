044c429b2eba17b1d72f55f342e856f4ada10c97
Ashish Poudel <ashishpoudel523@gmail.com>
2022-05-28 20:37:39 +0545

    models nodejs

--- app.js
+++ app.js
@@ -5,3 +5,29 @@ function add(a, b) {
 
 
 module.exports = add
+
+
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    fullname: DataTypes.STRING,
+    location: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
